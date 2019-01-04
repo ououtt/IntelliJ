fd3982ba36034fd365ffb569485699af44364cc6
guzemin <guzemin@songxiaocai.com>
2019-01-04 23:51:14 +0800

    IU-2018.3.2 <gzm@gzmdembp.lan Create baseRefactoring.xml, markdown.xml, ide.general.xml, project.default.xml, Enum.java, Interface.java, lombok-plugin.xml, git.xml, other.xml, Default.xml, find.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, javaeeExternalResources.xml, remote-servers.xml, plugin_ui.xml, colors.scheme.xml, _@user_Darcula.icls, VisualVMLauncher.xml, path.macros.xml, databaseDrivers.xml, mavenVersion.xml, AnnotationType.java, AiXCoderConfig.xml, Default.xml, Class.java, jdk.table.xml, laf.xml, Mac OS X 10_5_ copy.xml, vcs.xml, grepConsole.xml, databaseSettings.xml, toStringTemplates.xml, keymap.xml, mybatis.xml, editor.xml, usageView.xml, debugger.xml, sonarlint.xml, updates.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+* @author guzemin@songxiaocai.com
+* @date ${YEAR}-${MONTH}-${DAY} ${TIME}
+**/
+public interface ${NAME} {
+}
