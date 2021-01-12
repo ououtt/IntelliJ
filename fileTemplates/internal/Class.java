f64e2a1e564eee380ece363b881a59abacde1b61
guzemin <zemingu@91jkys.com>
2021-01-12 21:36:54 +0800

    IU-2020.1.2 <gzm@gzmdeMacBook-Pro.local Overwrite remote https://github.com/ououtt/IntelliJ to local

--- fileTemplates/internal/Class.java
+++
@@ -1,9 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#parse("File Header.java")
-
-/**
-* @author guzemin@songxiaocai.com
-* @date ${YEAR}-${MONTH}-${DAY} ${TIME}
-**/
-public class ${NAME} {
-}
